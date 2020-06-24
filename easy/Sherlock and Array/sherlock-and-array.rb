cfefcaac136e0cee2b50697306e56fd7d20b3a39
wosami <osami0247@gmail.com>
2020-06-25 09:08:56 +1200

    Sample test is ok but take too much time.

--- easy/Sherlock and Array/sherlock-and-array.rb
+++ easy/Sherlock and Array/sherlock-and-array.rb
@@ -5,8 +5,34 @@ require 'stringio'
 
 # Complete the balancedSums function below.
 def balancedSums(arr)
-
-
+    n = arr.size
+    i = n - 2
+    right_all = arr.slice(1..-1).sum
+    if right_all == 0
+        result = 'YES'
+    else
+        i.times do |j|
+            if j == i
+                left_all = arr.slice(1..-1).sum
+                if left_all == 0
+                    result = 'YES'
+                    break
+                else
+                    result = 'NO'
+                end
+            else
+                arr_left = arr.slice(0..j)
+                arr_right =arr.slice(j+2..-1)
+                if arr_left.sum == arr_right.sum
+                    result = 'YES'
+                    break
+                else
+                    result = 'NO'
+                end
+            end
+        end
+    end
+    return result
 end
 
 #fptr = File.open(ENV['OUTPUT_PATH'], 'w')
@@ -14,7 +40,7 @@ fptr = File.open('/myapp/openfile', 'w')
 
 T = gets.strip.to_i
 
-T.times do |T_itr|
+T.times do |t_itr|
     n = gets.strip.to_i
 
     arr = gets.rstrip.split.map(&:to_i)
