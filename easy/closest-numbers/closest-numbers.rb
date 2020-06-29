e73c583c8b3f083ba8f403f73f6bcd5b437d3e88
wosami <osami0247@gmail.com>
2020-06-30 09:29:12 +1200

    Complete

--- easy/closest-numbers/closest-numbers.rb
+++ easy/closest-numbers/closest-numbers.rb
@@ -4,9 +4,21 @@ require 'json'
 require 'stringio'
 
 # Complete the closestNumbers function below.
-def closestNumbers(arr)
-
-
+def closestNumbers(n, arr)
+    sorted_arr = arr.sort
+    abs_arr = []
+    for i in 0..n-2 do
+        diff = sorted_arr[i] - sorted_arr[i+1]
+        #abs_arr << (sorted_arr[i] - sorted_arr[i+1]).abs
+        abs_arr << diff.abs
+    end
+    min_abs = abs_arr.min
+    ele_num = abs_arr.each_index.select{|i| abs_arr[i] == min_abs}
+    result = []
+    ele_num.each do |j|
+        result.push(sorted_arr[j], sorted_arr[j+1])
+    end
+    return result.sort
 end
 
 #fptr = File.open(ENV['OUTPUT_PATH'], 'w')
@@ -16,7 +28,7 @@ n = gets.to_i
 
 arr = gets.rstrip.split(' ').map(&:to_i)
 
-result = closestNumbers arr
+result = closestNumbers n, arr
 
 fptr.write result.join " "
 fptr.write "\n"
