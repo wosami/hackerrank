4a35c5c983953a03be7485847c000945e67126ba
wosami <osami0247@gmail.com>
2020-07-06 09:34:06 +1200

    think the process

--- medium/the-bomberman-game/the-bomberman-game.rb
+++ medium/the-bomberman-game/the-bomberman-game.rb
@@ -5,8 +5,15 @@ require 'stringio'
 
 # Complete the bomberMan function below.
 def bomberMan(n, grid)
-    
-
+    # process
+    # 1. check where is 0
+    # 2. do nothing 1 second
+    # 3. put 0 in all place after 2 second
+    # 4. remove where was 0 and neibors of it and check where is 0
+    # 5. repeat 3-4
+    grid.each do |arr|
+        
+    end
 end
 
 #fptr = File.open(ENV['OUTPUT_PATH'], 'w')
@@ -26,6 +33,9 @@ r.times do |i|
     grid_item = gets.to_s.rstrip
     grid[i] = grid_item
 end
+
+# grid example
+# [[.,.,.,.,.,.,.],[.,.,.,O,.,.,.],..,[O,O,.,.,.,.,.]]
 
 result = bomberMan n, grid
 
