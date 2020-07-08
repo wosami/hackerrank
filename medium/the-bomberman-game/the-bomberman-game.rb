40785f81d6907588290d2fee6a71b7474347ba8a
wosami <osami0247@gmail.com>
2020-07-09 10:37:16 +1200

    Pass some test. Still have problem of runtime error

--- medium/the-bomberman-game/the-bomberman-game.rb
+++ medium/the-bomberman-game/the-bomberman-game.rb
@@ -6,101 +6,93 @@ require 'stringio'
 # Complete the bomberMan function below.
 def bomberMan(n, grid)
     # process
-    # 1. check where is 0
+    # 1. check where is O
     # 2. do nothing 1 second
-    # 3. put 0 in all place after 2 second
-    # 4. remove where was 0 and neibors of it and check where is 0
+    # 3. put O in all place after 2 second
+    # 4. remove where was O and neibors of it and check where is O
     # 5. repeat 3-4
     if n == 1
         return grid
     elsif n % 2 == 0
         grid.each do |grid_row|
             grid_row.length.times do |i|
-                grid_row[i] = "0"
+                grid_row[i] = "O"
             end
         end
         return grid
     else
-        # n = n-2
-        # n.times do
-        hash_zeros = {}
-        index = 0
-        grid.each do |grid_row|
-            row_arr = grid_row.rstrip.split(//)
-            # p row_arr
-            # get index number where is 0 from grid_row
-            #elements = row_arr.each_index.select{|i| grid_row[i] == "0"}
-            if row_arr.include?("O")
-                elements = []
-                row_arr.each_index {|i|
-                    if row_arr[i].to_s == "O"
-                        elements << i
-                    end
-                }
-                # p elements
-                hash_zeros[index] = elements
-            else
-                hash_zeros[index] = "none"
-            end
-            # p index
-            index += 1
-        end
-        grid.each do |grid_row|
-            grid_row.length.times do |i|
-                grid_row[i] = "0"
-            end
-        end
-        hash_zeros.each do |k, v|
-            if v != "none"
-                v.each do |j|
-                    if k == 0
-                        grid[k][j] = "."
-                        grid[k+1][j] = "."
-                        if j == 0
-                            grid[k][j+1] = "."
-                        elsif j == 5
-                            grid[k][j-1] = "."
-                        else
-                            grid[k][j+1] = "."
-                            grid[k][j-1] = "."
+        n = (n-1)/2
+        n.times do
+            # p grid
+            hash_zeros = {}
+            index = 0
+            grid.each do |grid_row|
+                row_arr = grid_row.rstrip.split(//)
+                # p row_arr
+                # get index number where is 0 from grid_row
+                #elements = row_arr.each_index.select{|i| grid_row[i] == "0"}
+                if row_arr.include?("O")
+                    elements = []
+                    row_arr.each_index {|i|
+                        if row_arr[i].to_s == "O"
+                            elements << i
                         end
-                    elsif k == 5
+                    }
+                    # p elements
+                    hash_zeros[index] = elements
+                else
+                    hash_zeros[index] = "none"
+                end
+                # p index
+                index += 1
+            end
+            grid.each do |grid_row|
+                grid_row.length.times do |i|
+                    grid_row[i] = "O"
+                end
+            end
+            r = grid.length
+            c = grid[0].length
+            hash_zeros.each do |k, v|
+                if v != "none"
+                    v.each do |j|
                         grid[k][j] = "."
-                        grid[k-1][j] = "."
-                        if j == 0
-                            grid[k][j+1] = "."
-                        elsif j == 5
-                            grid[k][j-1] = "."
+                        if k == 0
+                            grid[k+1][j] = "."
+                            if j == 0
+                                grid[k][j+1] = "."
+                            elsif j == c - 1
+                                grid[k][j-1] = "."
+                            else
+                                grid[k][j+1] = "."
+                                grid[k][j-1] = "."
+                            end
+                        elsif k == r - 1
+                            grid[k-1][j] = "."
+                            if j == 0
+                                grid[k][j+1] = "."
+                            elsif j == c -1
+                                grid[k][j-1] = "."
+                            else
+                                grid[k][j+1] = "."
+                                grid[k][j-1] = "."
+                            end
                         else
-                            grid[k][j+1] = "."
-                            grid[k][j-1] = "."
-                        end
-                    else
-                        grid[k][j] = "."
-                        grid[k-1][j] = "."
-                        grid[k+1][j] = "."
-                        if j == 0
-                            grid[k][j+1] = "."
-                        elsif j == 5
-                            grid[k][j-1] = "."
-                        else
-                            grid[k][j+1] = "."
-                            grid[k][j-1] = "."
+                            grid[k-1][j] = "."
+                            grid[k+1][j] = "."
+                            if j == 0
+                                grid[k][j+1] = "."
+                            elsif j == c -1
+                                grid[k][j-1] = "."
+                            else
+                                grid[k][j+1] = "."
+                                grid[k][j-1] = "."
+                            end
                         end
                     end
                 end
             end
         end
-    # i,j = 0
-    # grid.each do |arr|
-    #     arr.each do |e|
-    #         if e == 0
-    #             zeros << [i, j]
-    #         end
-    #         j++
-    #     end
-    #     i++
-    # end
         return grid
     end
 end
