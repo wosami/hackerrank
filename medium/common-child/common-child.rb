52d67c743443f79363b2db8a65fe589517a9ceaf
wosami <osami0247@gmail.com>
2020-07-11 09:40:02 +1200

    Plan process

--- medium/common-child/common-child.rb
+++ medium/common-child/common-child.rb
@@ -5,7 +5,14 @@ require 'stringio'
 
 # Complete the commonChild function below.
 def commonChild(s1, s2)
-
+    # process
+    #1: loop for each string of 1st line
+    #2: check if same string is in 2nd line array
+    #3: add string to the array
+    #4: slice 2nd array where is same string and end loop
+    #5: check if next string is in the sliced 2nd array
+    #6: make array of length of the string
+    #7: return maximum of length from array
 
 end
 
