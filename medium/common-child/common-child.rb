bc448909439e19008c9765d7bfd39168e544e087
wosami <osami0247@gmail.com>
2020-07-14 09:55:44 +1200

    Added conditions for when no intersection

--- medium/common-child/common-child.rb
+++ medium/common-child/common-child.rb
@@ -13,7 +13,24 @@ def commonChild(s1, s2)
     #5: check if next string is in the sliced 2nd array
     #6: make array of length of the string
     #7: return maximum of length from array
-
+    arr_s1 = s1.split(//)
+    arr_s2 = s2.split(//)
+    common = arr_s1 & arr_s2
+    commons = []
+    if common.length != 0
+        arr_s1.each do |s|
+            p s
+            if arr_s2.include?(s)
+                commons << s
+                num = arr_s2.index(s)
+                arr_s2 = arr_s2.slice(num+1..-1)
+                p arr_s2
+            end
+        end
+    else
+        return 0
+    end
+    return commons.length
 end
 
 #fptr = File.open(ENV['OUTPUT_PATH'], 'w')
